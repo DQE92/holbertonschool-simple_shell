15c0fd7f55cf772e9595e607b4d0a515134bb2e1
Camilledct <camilledct28@gmail.com>
2024-12-17 14:24:34 +0100

    add AUTHORS.sh

---
+++ AUTHORS.sh
@@ -0,0 +1,4 @@
+# This file lists all individuals having contributed content to the repository.
+# For how it is generated, see 'generate-authors.sh'.
+Ducatte Camille <camilledct28@gmail.com>
+Duque Dylan <duquedylan@icloud.com>
