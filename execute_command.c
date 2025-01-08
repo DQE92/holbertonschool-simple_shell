13f99a4b39693dfd420c2e2e0f6f7be3230110d6
Camilledct <camilledct28@gmail.com>
2025-01-08 01:24:01 +0100

    Add args handling

--- execute_command.c
+++ execute_command.c
@@ -1,18 +1,31 @@
 #include "shell.h"
+#include <string.h> /* Pour strtok */
 
 /**
  * execute_command - Execute a command via execve
- * @command: command to execute
- * @program_name: program name
+ * @command: Command to execute
+ * @program_name: Name of the main program
  */
 void execute_command(char *command, char *program_name)
 {
 	pid_t pid;
 	int status;
-	char *args[2];
+	char *args[1024]; /* Tableau pour stocker les arguments (limité à 1024) */
+	int i = 0;
 
-	args[0] = command;
-	args[1] = NULL;
+	/* Découper la commande en arguments avec strtok */
+	char *token = strtok(command, " ");
+
+	while (token != NULL && i < 1023) /* Limiter le nombre d'arguments à 1023 */
+	{
+		args[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL; /* Le dernier élém du tab doit être NULL pour execve */
+
+	if (args[0] == NULL) /* Si aucune commande n'a été saisie, retourner */
+		return;
 
 	pid = fork();
 	if (pid == -1) /* Gestion d'erreur pour fork */
@@ -22,11 +35,11 @@ void execute_command(char *command, char *program_name)
 	}
 	else if (pid == 0) /* Processus enfant */
 	{
-		if (execve(command, args, environ) == -1)
+		if (execve(args[0], args, environ) == -1) /* Exécuter la commande */
 		{
-			/* fprintf(stderr, "%s: 1: %s: not found\n", program_name, command); */
+			/* Si execve échoue, afficher une erreur et quitter avec code 127 */
 			fprintf(stderr, "%s: No such file or directory\n", program_name);
-			exit(127); /* Commande introuvable */
+			exit(127);
 		}
 	}
 	else /* Processus parent */
